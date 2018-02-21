ad2b7f8eddaca9ecddea88714cd68b92ee947e3b
Congjian Wang <congjian.wang@inl.gov>
2018-02-21 15:14:44 -0700

    fix the metrics used in data mining

--- framework/PostProcessors/DataMining.py
+++ framework/PostProcessors/DataMining.py
@@ -33,6 +33,7 @@ from utils import InputData
 import Files
 import unSupervisedLearning
 import Runners
+import MetricDistributor
 #Internal Modules End-----------------------------------------------------------
 
 class DataMining(PostProcessor):
@@ -616,7 +617,10 @@ class DataMining(PostProcessor):
     """
     self.unSupervisedEngine.features = Input['Features']
     if not self.unSupervisedEngine.amITrained:
-      self.unSupervisedEngine.train(Input['Features'], self.metric)
+      metric = None
+      if self.metric !=None:
+        metric = MetricDistributor.returnInstance('MetricDistributor', self.metric, self)
+      self.unSupervisedEngine.train(Input['Features'], metric)
     self.unSupervisedEngine.confidence()
 
     self.userInteraction()
